4b54244cb929587d783706260392ecca4617c54c
Kevin <giangcamvinh@gmail.com>
2021-03-28 11:42:45 -0700

    add counter, first livewire component

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,8 @@
+<div>
+     If you look to others for fulfillment, you will never truly be fulfilled.
+    <div class="" style="text-align: center">
+        <button wire:click="decrement">-</button>
+        <span>{{ $count }}</span>
+        <button wire:click="increment">+</button>
+    </div>
+</div>
