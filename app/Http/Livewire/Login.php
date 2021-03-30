bd0a6c2f91698c78330bb6d4e7020263dc3c0796
Kevin <giangcamvinh@gmail.com>
2021-03-29 22:10:44 -0700

    add login feature

--- app/Http/Livewire/Login.php
+++ app/Http/Livewire/Login.php
@@ -2,12 +2,29 @@
 
 namespace App\Http\Livewire;
 
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class Login extends Component
 {
+    public $form = [
+        'email'    => '',
+        'password' => ''
+    ];
+
     public function render()
     {
         return view('livewire.login');
+    }
+
+    public function submit() {
+        $this->validate([
+            'form.email' => 'required|email',
+            'form.password' => 'required'
+        ]);
+
+        Auth::attempt($this->form);
+
+        return redirect('/');
     }
 }
